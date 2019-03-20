19b59e0c1415f5c0079e84d7e895924a55cfe55d
tuzmusic <tuzmusic@gmail.com>
2019-03-20 17:53:28 -0400

    enter by hand

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStore, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
+import axios from "axios";
+import axiosMiddleware from "redux-axios-middleware";
+
+import reducer from "./src/reducer";
+import RepoList from "./src/RepoList";
+
+const client = axios.create({
+  baseURL: "https://api.github.com",
+  responseType: "json"
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <RepoList />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
