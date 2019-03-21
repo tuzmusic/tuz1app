81df9b6ec3ad08cecc51d1ddb7952eacee8c6eb0
tuzmusic <tuzmusic@gmail.com>
2019-03-21 09:55:49 -0400

    add Tabs component

---
+++ src/components/Tabs.js
@@ -0,0 +1,8 @@
+import { TabNavigator } from "react-navigation";
+import RepoList from "./RepoList";
+import Profile from "./Profile";
+
+const Tabs = TabNavigator({
+  RepoList: { screen: RepoList },
+  Profile: { screen: Profile }
+})
